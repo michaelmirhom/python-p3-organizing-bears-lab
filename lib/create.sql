c5d1cdabdf6fa70d9e9b5f0df7fb1e04d3ac207b
michaelmirhom <micharby73@gmail.com>
2023-08-09 09:32:30 -0500

    completed

---
+++ lib/create.sql
@@ -0,0 +1,5 @@
+CREATE TABLE bears(
+id INTEGER , 
+name TEXT, age INTEGER , 
+sex TEXT, color TEXT,  
+ temperament TEXT,alive  BOOLEAN); 
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'Black', 'Rowdy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'White', 'Gentle', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'M', 'Brown', 'Strict', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'Grey', 'Friendly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'Green', 'Grumpy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'Yellow', 'Cheerful', 1);
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES (3, 'F', 'Brown', 'Mysterious', 1);
+
+
